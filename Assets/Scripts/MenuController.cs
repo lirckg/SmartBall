4100404778c2aa4896da7a034ff420347a6ad2cf
Ashidariku <lirckm@gmail.com>
2017-02-11 21:27:48 +0900

    SmartBallプロジュエクト　更新
    Menu画面ON

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour {
+
+	private GameObject menu;
+
+	// Use this for initialization
+	void Start () {
+		this.menu = GameObject.Find ("MENU");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// Menuボタンが押されたとき
+	public void GetMenuButtonDown(){
+		this.menu.transform.localPosition = new Vector3 (0, -150, 0);
+	}
+}
--- Assets/Scripts/StageSelectController.cs
+++ Assets/Scripts/StageSelectController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Security.Cryptography;
+using DG.Tweening;
+using System;
 
 
 public class StageSelectController : MonoBehaviour {
@@ -9,14 +11,20 @@ public class StageSelectController : MonoBehaviour {
 	private GameObject img;
 
 	// stagePrefabを入れる
-	public GameObject stagePrefab;
+	public GameObject stagePrefabRight;
+	public GameObject stagePrefabLeft;
 
+	// 初期img
 	private string[] stageName = { "StageLEFT", "StageMIDDLE", "StageRIGHT"};
 
 	// stageList
 	private List<GameObject> stageList = new List<GameObject> ();
 
+	// canvas オブジェクト
 	private GameObject canvas;
+
+	// 遷移方向
+	private float direction = 1.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -33,21 +41,51 @@ public class StageSelectController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		// 左矢印で左に
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			this.direction = 1.0f;
+		}
+		// 右矢印で右に
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			this.direction = -1.0f;
+		}
+
+		Debug.Log (this.direction);
+
+
 		if (Input.GetMouseButtonDown (0)) {
 
 			// 移動
 			for (int i = 0; i < stageList.Count; i++) {
 				GameObject stage = stageList [i];
-				stage.transform.Translate ( -300, 0, 0);
+				//stage.transform.Translate ( -300, 0, 0);
+
+				stage.transform.DOLocalMoveX (stage.transform.localPosition.x - (this.direction * 300f), 0.5f);
 			}
 
 			// 消去
-			GameObject firstObject = stageList [0];
-			this.stageList.Remove (firstObject);
-			Destroy (firstObject);
+			GameObject destroyObject;
+			int argMax = 0;
+			int argMin = 0;
+
+			for (int i = 0; i < stageList.Count; i++) {
+				if (stageList [i].transform.localPosition.x == -300)
+					argMin = i;
+				if (stageList [i].transform.localPosition.x ==  300)
+					argMax = i;
+			}
+
+			// 遷移方向
+			if (direction == 1.0f) {
+				destroyObject = stageList [argMin];
+
+			} else {
+				destroyObject = stageList [argMax];
+			}
+			this.stageList.Remove (destroyObject);
+			Destroy (destroyObject);
 
 			// 作成
-
 			GameObject newStage = makeNewStage ();
 			this.stageList.Add (newStage);
 
@@ -61,10 +99,18 @@ public class StageSelectController : MonoBehaviour {
 
 
 	GameObject makeNewStage(){
+		GameObject newStage;
 
-		// stageインスタンスを作成
-		GameObject newStage = Instantiate (stagePrefab) as GameObject;
+		if (this.direction == 1.0f) {
+			// stageインスタンスを作成
+			newStage = Instantiate (stagePrefabRight) as GameObject;
+		} else {
+			newStage = Instantiate (stagePrefabLeft) as GameObject;
+		}
+
 		newStage.transform.parent = this.canvas.transform;
+
+
 
 		return newStage;
 
